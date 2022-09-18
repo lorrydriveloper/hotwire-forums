de11d386f2348dce0dff409571632fbd4477f0b1
lorrydriveloper <lorrydriveloper@gmail.com>
2022-09-18 21:44:06 +0100

    set-up navbar.

--- test/controllers/main_controller_test.rb
+++ test/controllers/main_controller_test.rb
@@ -1,8 +1,10 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class MainControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get main_index_url
+  test 'should get index' do
+    get root_url
     assert_response :success
   end
 end
