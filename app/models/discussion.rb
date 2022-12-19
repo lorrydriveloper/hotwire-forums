8331899da4339b20ff1146cbaf8e54fdfe9b3f8b
lorrydriveloper <lorrydriveloper@gmail.com>
2022-12-19 20:54:14 +0000

    holy-sit

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Discussion < ApplicationRecord
-  belongs_to :user, default: -> { Current.user }
+  belongs_to :user, default: -> { Current.user || 'Holy-sit' }
 
   def self.to
     'something'
