6c84d67fa7ee12c6dc2e72fbbdfd1fb14372ce64
lorrydriveloper <lorrydriveloper@gmail.com>
2022-12-19 20:49:12 +0000

    remove crappy code.

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Discussion < ApplicationRecord
-  belongs_to :user, default: -> { Current.user || nil }
+  belongs_to :user, default: -> { Current.user }
 
   def self.to
     'something'
