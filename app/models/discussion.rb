27cf5573667971e5435d0b641d93b0d51bf62559
lorrydriveloper <lorrydriveloper@gmail.com>
2022-12-19 20:10:54 +0000

    more stuff

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -3,6 +3,10 @@
 class Discussion < ApplicationRecord
   belongs_to :user, default: -> { Current.user }
 
+  def self.to
+    'something'
+  end
+
   validates :name, presence: true
 
   def other_thing
